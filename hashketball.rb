df428d3a9a71090516c14cdb1f5361b87adea8b9
stephen pesce <stevepesce879@gmail.com>
2020-05-10 18:45:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,28 +1,43 @@
 require 'pry'
 def game_hash
+  NETS_PLAYERS = 
+  [
+    player("Alan Anderson", "0", "16", 22, 12, 12, 3, 1 ,1),
+    player("Reggie Evans" , "30", "14")
+  ]
+end
+
+#return team with details and empty player array
+def team_helper
   top = 
   {
     :home => {},
     :away => {}
   }
-  mid = 
+  mid_Nets = 
   {
-    :team    => "",
-    :colors  => [],
+    :team    => "Brooklyn Nets",
+    :colors  => ['Black', 'White'],
     :players => []
   }
-  top.each{|key, value|
-    top[key] = mid
+  mid_Hornets =
+  {
+    :team    => "Charlotte Hornets",
+    :colors  => ["Turquoise", "Purple"],
+    :players => []
   }
+  top[:home] = mid_Nets
+  top[:away] = mid_Hornets
   
 end
 
-def player_helper(name, number, shoe, points, rebounds, assits, steals, blocks, slam_dunks)
+#create hash for each player
+def player(name, number, shoe, points, rebounds, assits, steals, blocks, slam_dunks)
   player = 
   {
     :player_name => name,
-    :number      => number,
-    :shoe        => shoe,
+    :number      => number.to_s,
+    :shoe        => shoe.to_s,
     :points      => points,
     :rebounds    => rebounds,
     :assists     => assists,
